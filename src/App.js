7e63a6491f3b4e904963dc2c07fcd11f3e858ddd
James Troughton <jamesstroughton@gmail.com>
2020-09-27 18:42:42 +1300

    Header Progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <Header />
+      {/* home */}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDPYxVfb9zkmlteT4MiBPPO2MIJVfMJIYc",
+    authDomain: "ecommerce-store-b35d5.firebaseapp.com",
+    databaseURL: "https://ecommerce-store-b35d5.firebaseio.com",
+    projectId: "ecommerce-store-b35d5",
+    storageBucket: "ecommerce-store-b35d5.appspot.com",
+    messagingSenderId: "649313552246",
+    appId: "1:649313552246:web:6d224dd8a7fd8bcb359c57",
+    measurementId: "G-BPH3JZ07V7"
+  };
